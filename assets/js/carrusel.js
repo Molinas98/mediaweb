276bf260c3a30019de7d130e8ebddab18ad7ed02
Tadeo <tade25.molinas@gmail.com>
2025-05-13 15:22:09 -0300

    hola

--- assets/js/carrusel.js
+++ assets/js/carrusel.js
@@ -106,15 +106,5 @@ $(document).ready(function () {
         ]
     });
 
-    
-    $('.popup-video').magnificPopup({
-        type: 'iframe',
-        mainClass: 'mfp-fade',
-        removalDelay: 160,
-        preloader: false,
-    
-        fixedContentPos: true
-    });
-
     AOS.refresh();
 });
